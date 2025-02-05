e36732d72a1cebb76b9474f08f128ed445f8f231
Brijesh1990 <bkpandey.pandey@gmail.com>
2025-02-05 14:56:02 +0530

    05-02-2025 all data uploaded

---
+++ reactjs/module3-reactjs-installations/bootstrap/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+ 
+function App(){
+  return(
+    <>
+      <p className='w-50 mx-auto bg-danger text-white p-5 mt-5'>
+        This is bootstrap
+      </p>
+      <button className='btn btn-primary btn1 w-50'>Click me</button>
+    </>
+  )
+}
+
+export default App
