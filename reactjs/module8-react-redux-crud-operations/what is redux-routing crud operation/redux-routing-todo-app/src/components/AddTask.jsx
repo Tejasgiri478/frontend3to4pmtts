af44835cd56f1d9a40c5c1447c7a03bfe56f141e
Brijesh1990 <bkpandey.pandey@gmail.com>
2025-04-29 15:16:33 +0530

    29-04-2025 all data uploaded

--- reactjs/module8-react-redux-crud-operations/what is redux-routing crud operation/redux-routing-todo-app/src/components/AddTask.jsx
+++ reactjs/module8-react-redux-crud-operations/what is redux-routing crud operation/redux-routing-todo-app/src/components/AddTask.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './partials/Header'
 import Navbar from './partials/Navbar'
+import { useSelector,useDispatch } from 'react-redux'
+// import all function from todoSlice components
+import { addTodo,toggleTodo,deleteTodo } from '../fetaures/todos/todoSlice'
+
 const AddTask=()=> {
+// take input from user 
+const[input,setInput]=useState("");
+const todos=useSelector((state)=>state.todos);
+const dispatch=useDispatch();
+
+// create a form handeling function 
+const handeleAdd=(e)=>{
+     e.preventDefault();
+    if(input.trim()){
+        dispatch(addTodo(input));
+        setInput('');
+    }
+} 
+
 return (  
 <>
 <Header />
@@ -12,11 +30,35 @@ return (
 <p className='fs-5'>Lets add some to do app via redux / toolkit </p>
 <form className='mt-2'>
 <div className='input-group'>
-    <input type='text' placeholder='Add here something for add in List *' className='form-control' />
-    <button type='button' className='btn btn-sm btn-dark text-white w-25'><span className='bi bi-plus fs-4'></span></button>
+    <input type='text' value={input} onChange={(e)=>setInput(e.target.value)} placeholder='Add here something for add in List *' className='form-control' />
+    <button  className='btn btn-sm btn-dark text-white w-25' onClick={handeleAdd}><span className='bi bi-plus fs-4'></span></button>
 </div>
 </form>
 
+{/* display the data */}
+
+<ul className='mt-5 m-5'>
+   {/* {todos.map((todo)=>{
+    return(
+        <>
+        <li key={todo.id}>{todo.text}</li>
+        </>
+    )
+   
+   })} */}
+
+   {todos.map((todo)=>(
+        <li key={todo.id} style={{textDecoration:todo.completed ? 'line-through':'none'}} className='mt-2'>{todo.text}
+
+        <button type='button' className='btn btn-sm btn-primary ms-2' onClick={()=>dispatch(toggleTodo(todo.id))}>Toggle</button>
+        
+        <button type='button' className='btn btn-sm btn-danger ms-2' onClick={()=>dispatch(deleteTodo(todo.id))}>Delete</button>
+
+        </li>
+   ))}
+
+
+</ul>
 </div>
 </>
 )
--- reactjs/module8-react-redux-crud-operations/what is redux-routing crud operation/redux-routing-todo-app/src/store.jsx
+++ reactjs/module8-react-redux-crud-operations/what is redux-routing crud operation/redux-routing-todo-app/src/store.jsx
@@ -1,32 +1,43 @@
-import { legacy_createStore } from "redux"
+// import { legacy_createStore } from "redux"
+import { configureStore } from "@reduxjs/toolkit";
+import todosReducer from './fetaures/todos/todoSlice'
 
-const initialState= {
- count: 0
-};
 
-const reducer=(state=initialState,action)=>{
+// counter app
+// const initialState= {
+//  count: 0
+// };
 
- switch(action.type) 
- {
-  case 'INCREMENT':
-   return{
-   ...state,
-   count:state.count + 1
-   };
+// const reducer=(state=initialState,action)=>{
+
+//  switch(action.type) 
+//  {
+//   case 'INCREMENT':
+//    return{
+//    ...state,
+//    count:state.count + 1
+//    };
    
-   case 'DECREMENT':
-   return{
-   ...state,
-   count:state.count - 1
+//    case 'DECREMENT':
+//    return{
+//    ...state,
+//    count:state.count - 1
    
-   };
+//    };
 
-  default:
-  return state
+//   default:
+//   return state
  
- 
- }
+//  }
 
-}
-const store=legacy_createStore(reducer);
+// }
+
+// for to do list 
+export const store=configureStore({
+    reducer :{
+        todos:todosReducer,
+    }
+})
+
+// const store=legacy_createStore(reducer);
 export default store
